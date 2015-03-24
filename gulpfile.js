5f413964bb526a7e9b6375c1f92a436bdc312769
Simon Holloway <holloway.sy@gmail.com>
2015-03-24 01:44:03 +0000

    use the vinyl-source-stream

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var babelify = require('babelify');
-var through2 = require('through2');
+var source = require('vinyl-source-stream');
 var browserSync = require('browser-sync');
 var config = require('config');
 var reload = browserSync.reload;
 var browserSyncActive = false;
 var paths = {
+	scriptMain : './src/app.js',
 	scripts : './src/**/*.js',
 	views : './src/**/*.html'
 };
@@ -30,26 +31,16 @@ gulp.task('views', function () {
 
 gulp.task('scripts', function () {
 
-	var babelifyIt = through2.obj(function (file, enc, next){
-		browserify({
-				entries : file.path,
-				debug: true 
-			})
-			.transform(babelify)
-			.bundle(function(err, res){
-				// assumes file.contents is a Buffer
-				if (err) {
-					console.log(err.message);
-					this.end();
-				} else {			
-					file.contents = res;
-				}
-				next(null, file);
-			});
-	});
+	var handelErrors = function(error) {
+		console.log(error.message);
+		this.emit('end');
+	};
 
-	var stream = gulp.src('./src/app.js')
-		.pipe(babelifyIt)
+	var stream = browserify(paths.scriptMain, {debug: true})
+		.transform(babelify)
+		.bundle()
+		.on('error', handelErrors)
+	    .pipe(source('app.js'))
 		.pipe(gulp.dest('build'))
 
 	if (browserSyncActive) {
